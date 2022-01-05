be89482a9f8b68f2ad47fc71283b03f2a9e32288
Arran <93921912+breakabridge@users.noreply.github.com>
2022-01-05 15:44:25 +0000

    Added comments

--- soft.cpp
+++ soft.cpp
@@ -2,20 +2,22 @@
 #include <fstream>
 #include <cmath>
 
-#define SWAP(x0, x) {float * tmp = x0; x0 = x; x = tmp;}
 #define IX(i, j) ((i) + (width * j))
 
-const int frames = 900;
-const int width = 10;
-const int height = 10;
-const float droppingHeight = 10;
-const float stiffness = 100;
-const float damping = 10;
-const float grav = 1;
-const float dt = 0.01;
+// SIMULATION PARAMETERS
+const int frames = 900;	// FRAMES TO RENDER
+const int width = 10;	// WIDTH OF BODY IN NO. OF PARTICLES
+const int height = 10;	// HEIGHT
+const float droppingHeight = 10;	// THE HEIGHT FROM WHICH TO DROP THE BODY
+const float stiffness = 100;	// HOOKE STIFFNESS CONSTANT FOR THE SPRINGS
+const float damping = 10;	// DAMPING CONSTANT FOR THE SPRINGS
+const float grav = 1;	// GRAVITY
+const float dt = 0.01;	// TIMESTEP
 
 const int size = width * height;
 const int springSize = 4 * size - 3 * (width + height) + 2;
+
+// USEFUL VECTOR FUNCTIONS
 float scalarProduct2D(float * x, float * y)   {
     return x[0] * y[0] + x[1] * y[1];
 }
@@ -24,6 +26,17 @@ float vectorNorm(float * x)	{
 	return sqrt(scalarProduct2D(x, x));
 }
 
+/* MASS POINTS THAT MAKE UP THE BODY.
+--- MEMBER VARIABLES ---
+pos -- POSITION OF PARTICLE
+vel -- VELOCITY
+force -- TOTAL FORCE ON PARTICLE
+
+--- MEMBER FUNCTIONS ---
+CONSTRUCTOR Point(int x, int y) -- INITIALISES POINT WITH INITIAL POSITION [x, y]
+resetForces()	-- INITIALISES THE FORCES SO THAT ONLY GRAVITY IS ACTING
+update()	-- FORWARD EULER TIMESTEP TO CALCULATE NEXT POSITION
+*/
 class Point	{
 	public:
 		float pos[2];
@@ -50,10 +63,22 @@ class Point	{
 			pos[1] += (vel[1] + newVel[1]) * 0.5 * dt;
 			vel[0] = newVel[0]; vel[1] = newVel[1];
 
-			if (pos[1] < 0) {vel[1] -= 2 * vel[1]; force[0] = 0; force[1] = 1;}
+			if (pos[1] < 0) vel[1] *= -1;	// REFLECTS OFF GROUND
 		}
 };
 
+
+/* SPRINGS BETWEEN MASS POINTS. THERE ARE SPRINGS BETWEEN ALL IMMEDIATE AND DIAGONAL NEIGHBOURS.
+--- MEMBER VARIABLES ---
+*p1 -- POINTER TO FIRST MASS POINT
+*p2 -- POINTER TO SECOND
+l   -- REST LENGTH OF SPRING
+
+--- MEMBER FUNCTIONS ---
+CONSTRUCTOR Point(Point * p1, Point * p2) -- INITIALISES SPRING WITH MASSES AT RESPECTIVE POINTS.
+calculateForce()	-- CALCULATES HOOKE'S LAW FORCE WITH DAMPING AND ADDS TO EACH PARTICLE'S OVERALL FORCE. ALSO INCLUDES
+			SELF-COLLISION CHECKING
+*/
 class Spring	{
 	public:
 		Point * p1;
@@ -68,8 +93,8 @@ class Spring	{
 			p1 = point1; p2 = point2;
 			float pos1[2] = {(p1 -> pos)[0], (p1 -> pos)[1]};
 			float pos2[2] = {(p2 -> pos)[0], (p2 -> pos)[1]};
-			float relVector[2] = {pos2[0] - pos1[0], pos2[1] - pos1[1]};
-			l = vectorNorm(relVector);
+			float relVector[2] = {pos2[0] - pos1[0], pos2[1] - pos1[1]};	// RELATIVE POSITION
+			l = vectorNorm(relVector);	// SETS REST LENGTH
 		}
 
 		void calculateForce()	{
@@ -111,7 +136,7 @@ int main()  {
 		}
 	}
 	
-	int count = 0;
+	int count = 0;	// TO KEEP TRACK OF HOW MANY SPRINGS HAVE BEEN ADDED THUS FAR
 	float pos1[2], pos2[2];
 	for (i = 0; i < size; i++)	{
 		pos1[0] = body[i].pos[0]; pos1[1] = body[i].pos[1];
@@ -127,12 +152,12 @@ int main()  {
 	}
 	
 	std::ofstream file;
-    file.open("soft_sim.dat");
-    srand(time(NULL));
-    file << width << "," << height << "," << frames << "\n";
+   	file.open("soft_sim.dat");
+   	srand(time(NULL));
+   	file << width << "," << height << "," << frames << "\n";
 
 	for (i = 0; i < frames; i++)	{
-		for (int k = 0; k < 10; k++)	{
+		for (int k = 0; k < 10; k++)	{	// REPEATED 10 TIMES SO VIDEO ISN'T TOO SLOW
 			for (j = 0; j < springSize; j++)	{
 				springs[j].calculateForce();
 			}
